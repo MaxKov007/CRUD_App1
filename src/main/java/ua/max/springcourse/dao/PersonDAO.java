aa55e13115fc0c3eef4a8b9346a5fee16e7f5446
KovalchukMax <kovalchukmax@gmail.com>
2021-06-23 16:35:48 +0300

    added delete people (DELETE)

--- src/main/java/ua/max/springcourse/dao/PersonDAO.java
+++ src/main/java/ua/max/springcourse/dao/PersonDAO.java
@@ -17,27 +17,28 @@ public class PersonDAO {
         people.add(new Person(++PEOPLE_COUNT, "Tom"));
         people.add(new Person(++PEOPLE_COUNT, "Bob"));
         people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT,"Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
     }
 
-    public List<Person> index(){
+    public List<Person> index() {
         return people;
     }
 
-    public Person show(final int id){
+    public Person show(final int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
-    public void save(Person person){
+    public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
 
-    public void update(int id, Person updatePerson){
+    public void update(int id, Person updatePerson) {
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(updatePerson.getName());
     }
 
-
-
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
 }
